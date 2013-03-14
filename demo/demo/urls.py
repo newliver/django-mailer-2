facb18f9a210a680146c86b3e74da5f5dc601c87
aaloy <aaloy@apsl.net>
2013-03-15 00:56:32 +0100

    Now we can show e-mail in the admin

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -4,21 +4,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from demo.views import MailListView, MailDetailView, DownloadView
-from demo.views import MailHtmlDetailView
+from demo.views import IndexView
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', MailListView.as_view(), name='home'),
-    url(r'^mail/(?P<pk>\d+)/$', MailDetailView.as_view(),
-        name='mail_detail'),
-    url('^mail/attachment/(?P<pk>\d+)/(?P<firma>[0-9a-f]{32})/$',
-        DownloadView.as_view(),
-        name="mail_download"),
-    url('^mail/html/(?P<pk>\d+)/$',
-        MailHtmlDetailView.as_view(),
-        name="mail_html"),
-    # Uncomment the next line to enable the admin:
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^$', IndexView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
